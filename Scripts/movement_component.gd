e3a922ec39490c385915c97ff3f3a0fff6b3e2ec
Quentin MUSY <quentin.musy@gmail.com>
2025-09-17 15:32:29 +0100

    chore: fixed movement

--- Scripts/movement_component.gd
+++ Scripts/movement_component.gd
@@ -8,6 +8,7 @@ class_name MovementComponent
 @export var ground_deceleration_speed: float = 25.0
 @export var air_acceleration_speed: float = 10.0
 @export var air_deceleration_speed: float = 3.0
+@export var change_direction_multiplier: float = 5.0
 
 func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
 	#body.velocity.x = direction * speed
@@ -17,4 +18,10 @@ func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void
 	else:
 		velocity_change_speed = air_acceleration_speed if direction != 0 else air_deceleration_speed
 		
+	# If trying to move in the opposite direction of current velocity,
+	# accelerate faster to make direction change snappy
+	if direction != 0 and sign(direction) != sign(body.velocity.x) and body.velocity.x != 0:
+		velocity_change_speed *= change_direction_multiplier
+
+	
 	body.velocity.x = move_toward(body.velocity.x, direction * speed, velocity_change_speed)
