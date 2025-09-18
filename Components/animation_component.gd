95f626b32ae0223afb87a87e193db182423eb57b
Quentin MUSY <quentin.musy@gmail.com>
2025-09-18 13:17:16 +0100

    feat: beginning of a character with some animation

---
+++ Components/animation_component.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name AnimationComponent
+
+@export_subgroup("Nodes")
+@export var anim_tree: AnimationTree
+
+func handle_horiziontal_direction(h_direction: float) -> void:
+	
+	anim_tree["parameters/Idle/blend_position"] = Vector2(h_direction, 0)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,11 +6,13 @@ extends CharacterBody2D
 @export var input_component: InputComponent
 @export var movement_component: MovementComponent
 @export var jump_component: AdvancedJumpComponent
+@export var animation_component: AnimationComponent
 
 func _physics_process(delta: float) -> void:
 	gravity_component.handle_gravity(self, delta)
 	movement_component.handle_horizontal_movement(self, input_component.input_horizontal)
 	jump_component.handle_jump(self, input_component.get_jump_input(), input_component.get_jump_input_released())
+	animation_component.handle_horiziontal_direction(input_component.input_horizontal)
 	move_and_slide()
 
 #const SPEED = 300.0
